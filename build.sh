55d0dde181ba936de36426866e254e8a97df5d42
pavlovdo <pavlovdo@gmail.com>
2020-10-16 10:54:56 +0300

    create conf.d and docker

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-readonly PROJECT=pystormon
-readonly PRODUCTION_DIR=/etc/zabbix/externalscripts
-readonly CONTAINER_OS=centos
-readonly CONTAINER_OLD_ID=$(docker ps -q --filter ancestor="$CONTAINER_OS":"$PROJECT")
-
-docker stop "$CONTAINER_OLD_ID"
-docker build -t "$CONTAINER_OS":"$PROJECT" .
