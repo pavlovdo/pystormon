463250a834bca341e6bdecead80345825949e6f4
Denis Pavlov <pavlovdo@gmail.com>
2021-01-26 09:57:10 +0300

    add comment

--- configread.py
+++ configread.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-import configparser
+# Python module for load parameters from main config file
 
 
 def configread(conffile, section, *parameters):
 
+    import configparser
+
     # read configuration file
     config = configparser.RawConfigParser()
     config.read(conffile)
-    params = {}
+    params = dict()
 
-    # check presence parameters in config file
+    # check presence of parameters in config file
     for parameter in parameters:
         try:
             params[parameter] = config.get(section, parameter)
         except configparser.NoOptionError as err:
-            print (err, '. Please set ' + parameter + ' value in the ' +
-                   'configuration file ' + conffile)
+            print(
+                err, f"Please set {parameter} value in the configuration file {conffile}")
 
     return params
