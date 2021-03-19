7b25005a8eb1ac5ef0d3a841908697f32d38c9e3
Denis Pavlov <pavlovdo@gmail.com>
2021-03-19 16:35:10 +0300

    fix shared functions

--- functions.py
+++ functions.py
@@ -1,15 +1,75 @@
 #!/usr/bin/env python3
 
-# Python module for send alarms about exceptions to slack channel
+import os
+import requests
+import socket
 
-def slack_post(slack_hook, message, hostname, icon_emoji=':snake:'):
+from configread import configread
+from pyzabbix import ZabbixSender
+from socket import timeout
+from sys import stderr
 
-    import os
-    import requests
-    import sys
+# set project name as current directory name
+project = os.path.abspath(__file__).split('/')[-2]
 
-    requests.post(slack_hook, json={'username': os.uname()[1],
-                                    'icon_emoji': icon_emoji,
-                                    'text': 'ERROR in: \"' +
-                                    str(sys.argv[0]) + ', ' + hostname + ', '
-                                    + '\"\n' + message})
+# get config file name
+conf_file = (f'/etc/zabbix/externalscripts/{project}/{project}.conf')
+
+# read network device parameters from config and save it to dict
+nd_parameters = configread(conf_file, 'NetworkDevice',
+                           'slack_hook', 'zabbix_server')
+
+# get variables for error notifications
+slack_hook = nd_parameters['slack_hook']
+zabbix_server = nd_parameters['zabbix_server']
+
+hostname = socket.gethostname()
+
+
+def slack_post(software, message, icon_emoji=':snake:'):
+    """ post alarms to slack channel """
+
+    if slack_hook:
+        requests.post(slack_hook, json={'username': hostname,
+                                        'icon_emoji': icon_emoji,
+                                        'text': f'{project}_error: exception in {software}: {message}'})
+
+
+def zabbix_send(data, printing, software):
+    """ send packet of data to zabbix """
+
+    try:
+        zabbix_send_status = ZabbixSender(zabbix_server).send(data)
+        if printing:
+            print(
+                f'Status of sending data to zabbix:\n{zabbix_send_status}')
+    except timeout as error:
+        print(
+            (f'{project}_error: exception in {software}: Zabbix trapper socket timeout on {zabbix_server}: {error}. '
+                f'Check health status of zabbix server.'),
+            file=stderr)
+        slack_post(
+            software,
+            (f'Zabbix trapper socket timeout on {zabbix_server}: {error}. '
+                f'Check health status of zabbix server.'))
+        exit(1)
+    except ConnectionRefusedError as error:
+        print(
+            (f'{project}_error: exception in {software}: Zabbix trapper refused connection on {zabbix_server}: {error}. '
+                f'Check running of zabbix trappers and firewall trafic permissions.'),
+            file=stderr)
+        slack_post(
+            software,
+            (f'Zabbix trapper refused connection on {zabbix_server}: {error}. '
+                f'Check running of zabbix trappers and firewall trafic permissions.'))
+        exit(1)
+    except ConnectionResetError as error:
+        print(
+            (f'{project}_error: exception in {software}: Zabbix trapper reset connection on {zabbix_server}: {error}. '
+                f'Check running of zabbix trappers.'),
+            file=stderr)
+        slack_post(
+            software,
+            (f'Zabbix trapper reset connection on {zabbix_server}: {error}. '
+                f'Check running of zabbix trappers.'))
+        exit(1)
